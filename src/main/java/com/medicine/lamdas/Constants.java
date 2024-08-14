9b297fd278b844a6ef41a12efc748cc093aacdc6
oscar alvarado <proyectos.oscar-alvarado@outlook.com>
2024-08-13 23:00:36 -0500

    se configura la lamda de acuerdo a la guia de twilio y funciona correctamente

---
+++ src/main/java/com/medicine/lamdas/Constants.java
@@ -0,0 +1,6 @@
+package com.medicine.lamdas;
+
+public interface Constants {
+    String SMS = "sms";
+    String COLOMBIAN_INDICATIVE = "+57";
+}
--- src/main/java/com/medicine/lamdas/Handler.java
+++ src/main/java/com/medicine/lamdas/Handler.java
@@ -3,9 +3,14 @@ package com.medicine.lamdas;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.twilio.Twilio;
+import com.twilio.rest.verify.v2.service.Verification;
 
 public class Handler implements RequestHandler<SQSEvent, Void> {
 
+    public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
+    public static final String SERVICE_SID = System.getenv("TWILIO_SERVICE_SID");
     @Override
     public Void handleRequest(SQSEvent sqsEvent, Context context) {
         for (SQSEvent.SQSMessage msg : sqsEvent.getRecords()) {
@@ -17,7 +22,11 @@ public class Handler implements RequestHandler<SQSEvent, Void> {
 
     private void processMessage(SQSEvent.SQSMessage msg, Context context) {
         try {
-            context.getLogger().log("Processed message " + msg.getBody());
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Verification verification =
+                    Verification.creator(SERVICE_SID,  Constants.COLOMBIAN_INDICATIVE.concat(msg.getBody()), Constants.SMS)
+                            .create();
+            context.getLogger().log("codigo generado con el id: " + verification.getSid());
         } catch (Exception e) {
             context.getLogger().log("An error occurred");
             throw e;
